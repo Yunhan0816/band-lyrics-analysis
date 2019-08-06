da7619f345cdcb23793c7365d6d0226cd16e6359
Hannah Huang <yunhanh@bu.edu>
2019-08-06 14:48:59 +0800

    update

--- songs.py
+++ songs.py
@@ -15,22 +15,22 @@ def get_lyrics(url):
     request.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36")
     page = urllib.request.urlopen(request)
     soup = BeautifulSoup(page, "lxml")
-    lyrics = soup.find("div", class_ = "lyrics")
+    lyrics = soup.find("div", class_ = "lyrics") # web scraping
     #print(lyrics)
     return lyrics.text
 
-f2 = open('urls', 'w')
+f2 = open('urls', 'w+')
 
 for artist in artists:
     a = search(artist, outputfilename, client_access_token)
     urls = map(lambda t: t[3], a)
     print(artist, len(list(urls)))
-    f =  open('lyrics/' + artist, 'w') # write lyrics to folder
+    f =  open('lyrics/' + artist, 'w+') # write lyrics to folder
     f2. write(artist)
     for url in urls:
         lyrics = get_lyrics(url)
         f2.write(url)
         print(url)
-        f.write(lyrics)
+        f.write(lyrics.encode("utf8"))
     f.close()
 f2.close()
